efd94630f128afa85a1b60919d7d4917eb64a576
Tintapple <tintapple2@gmail.com>
2026-09-18 14:30:19 -0400

    cool new Java script

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+// This is a single comment and is ignored by the browser
+
+/*
+This is a 
+multi-line comment and is also ignored by the browser
+*/
+
+console.log('Hello, World!'); // Same as the print() function in Python. It prints to the console, which is useful for debugging.
+console.warn('be careful')
+console.error('this is an error')
+
+console.log('Tin Thai, I am 16 years old')
+console.warn('Tin Thai has to leave school at 2:30 pm on september 18, 2026')
+console.error('Tin Thai has to get a vaccine shot')
+
+//strings
+console.log("10")
+console.log("portland High school")
+
+// Numbers
+console.log(10)
+console.log(3.14)
+
+// Booleans
+console.log(true)
+console.log(false)
+
+// checking data types using "typeof" operator
+console.log(typeof 10)
+console.log(typeof "hello")
+console.log(typeof true)
+
+// Variables are used to store values for later use
+// must choose "let"(the val may change) or 
+// "const"(the val will not change)
+
+const schoolName="portland High School"
+let middleSchool="King Middle School"
+
+console.log(schoolName)
+console.log(middleSchool)
+
+// schoolName="Deering High School"
+middleSchool="Lincoln Middle School"
+
+console.log(schoolName)
+console.log(middleSchool)
+
+// Interactive User Input
+
+// alert(message)
